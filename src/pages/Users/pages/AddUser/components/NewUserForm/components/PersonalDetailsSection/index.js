af284c0123eb4e1e15a55abf4a23e27cf007f212
MuniruIssah <imuniru@st.ug.edu.gh>
2021-08-28 14:45:49 +0000

    Add an New User and misc

---
+++ src/pages/Users/pages/AddUser/components/NewUserForm/components/PersonalDetailsSection/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import InputBlock from '../../../InputBlock/InputBlock'
+const PersonalDetailsSection = ({
+  onChange,
+  first_name,
+  last_name,
+  phone_number,
+  password,
+  email,
+}) => {
+  return (
+    <div className="personalDetailsSection">
+      <InputBlock
+        label="First Name"
+        onChange={onChange}
+        value={first_name}
+        type="text"
+      />
+      <InputBlock
+        label="Last Name"
+        onChange={onChange}
+        value={last_name}
+        type="text"
+      />
+      <InputBlock
+        label="Phone Number"
+        onChange={onChange}
+        value={phone_number}
+        type="text"
+      />
+      <InputBlock
+        label="Email"
+        onChange={onChange}
+        value={email}
+        type="email"
+      />
+      <InputBlock
+        label="Password"
+        onChange={onChange}
+        value={password}
+        type="password"
+      />
+    </div>
+  );
+};
+
+export default PersonalDetailsSection;
